1387677049e391b53995d78390ac789928d39630
alibedirhan <alibedirhan.d@gmail.com>
2020-05-03 19:45:11 +0300

    yetenek kısmı bitti

--- yetenekler.php
+++ yetenekler.php
@@ -7,15 +7,31 @@
             $yetenekler = $DB_connect->prepare("SELECT * FROM yetenekler ORDER BY beceri_id DESC");
             $yetenekler ->execute();
             $yetenekcek = $yetenekler->fetchAll(PDO::FETCH_ASSOC);
+            $stun = $ayarcek['sayi'];; // 3
+            $count = count($yetenekcek); // 9
+            $array = [];
+            $c = 0;
+            for($i = 1; $i <= $stun; $i++){
+                $limit = $count/$stun;
+                if ($i == $stun){
+                    $limit = $count;
+                }
+                $array[] = array_slice($yetenekcek, $c, $limit);
+                $c += ($count/$stun);
+            }
+            ##die(var_dump($array));
+
         ?>
                     <div class="skills-bar">
-                        <?php foreach ($yetenekcek as $row): ?>
-                    <div class="col-md-6 w3-agile-skills-grid">
+                        <?php foreach($array as $item): ?>
+                    <div class="col-md-<?php echo 12/$stun ?> w3-agile-skills-grid">
                             <section class='bar'>
                                 <section class='wrap'>
                                     <div class='wrap_right'>
                                         <div class='bar_group'>
-                                            <div class='bar_group__bar thin' label='<?php echo $row['beceri_adi'];?>' show_values='true' tooltip='true' value='<?php echo $row['beceri_bar'];?>'></div>
+                                            <?php foreach ($item as $row):?>
+                                                <div class='bar_group__bar thin' label='<?php echo $row['beceri_adi'];?>' show_values='true' tooltip='true' value='<?php echo $row['beceri_bar'];?>'></div>
+                                            <?php endforeach; ?>
                                         </div>
                                     </div>
                                     <div class="clearfix"></div>
@@ -23,7 +39,6 @@
                             </section>
                         </div>
                         <?php endforeach; ?>
-
                     <div class="clearfix"></div>
                     </div>
     </div>
