54a72cdcb86000ebf471f4d004fa5ebe92d99845
alibedirhan <alibedirhan.d@gmail.com>
2020-05-06 19:40:10 +0300

    contact ve footer düzenlendi

--- contact.php
+++ contact.php
@@ -1,4 +1,7 @@
 <?php
+        $contact = $DB_connect ->prepare("SELECT * FROM iletisim");
+        $contact ->execute();
+        $contactadd = $contact
 ?>
 
 <div class="contact" id="contact">
@@ -9,15 +12,21 @@
         <div class="contact-w3ls">
             <form action="#" method="post">
                 <div class="col-md-7 col-sm-7 contact-left agileits-w3layouts">
-                    <input type="text" name="First Name" placeholder="Name" required="">
+                    <input type="text" name="First Name" placeholder="Ad" required="">
+                    <input type="text" name="Last Name" placeholder="Soyad" required="">
                     <input type="email"  class="email" name="Email" placeholder="Email" required="">
-                    <input type="text" name="Number" placeholder="Mobile Number" required="">
-                    <!-- <input type="text" class="email" name="Last Name" placeholder="Last Name" required=""> -->
+
                 </div>
                 <div class="col-md-5 col-sm-5 contact-right agileits-w3layouts">
-                    <textarea name="Message" placeholder="Message" required=""></textarea>
-                    <input type="submit" value="Submit">
+                    <textarea name="Message" placeholder="Mesaj" required=""></textarea>
+                    <input type="submit" value="Gönder">
                 </div>
+                        <footer class="w3-container w3-center w3-margin-top">
+
+                            <a href="<?php echo $ayarcek['sosyal_twitter']; ?>" target="_blank"><i class="fa fa-twitter w3-hover-opacity"></i></a>
+                            <a href="<?php echo $ayarcek['sosyal_linkedin']; ?>" target="_blank"><i class="fa fa-linkedin w3-hover-opacity"></i></a>
+
+                       </footer>
                 <div class="clearfix"> </div>
             </form>
         </div>
--- footer.php
+++ footer.php
@@ -3,16 +3,7 @@
 
 <div class="w3l_footer">
     <div class="w3l_footer_pos">
-        <footer class="w3-container w3-teal w3-center w3-margin-top">
-        <a href="<?php echo $smcek['sm_facebook'] ?>" target="_blank"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
-        <a href="<?php echo $smcek['sm_instgram'] ?>" target="_blank"><i class="fa fa-instagram w3-hover-opacity"></i></a>
-        <a href="<?php echo $smcek['sm_snapchat'] ?>" target="_blank"><i class="fa fa-snapchat w3-hover-opacity"></i></a>
-        <a href="<?php echo $smcek['sm_pinterest'] ?>" target="_blank"><i class="fa fa-pinterest-p w3-hover-opacity"></i></a>
-        <a href="<?php echo $smcek['sm_twitter'] ?>" target="_blank"><i class="fa fa-twitter w3-hover-opacity"></i></a>
-        <a href="<?php echo $smcek['sm_linkedin'] ?>" target="_blank"><i class="fa fa-linkedin w3-hover-opacity"></i></a>
-        <a href="<?php echo $smcek['sm_youtube'] ?>" target="_blank"><i class="fa fa-youtube w3-hover-opacity"></i></a>
 
-        </footer>
 
         <?php
 
