825adeb6b61c18982b7248bc742bca38d3891c53
alibedirhan <alibedirhan.d@gmail.com>
2020-05-14 19:35:10 +0300

    iletişim kısmı güncellendi

--- contact.php
+++ contact.php
@@ -1,10 +1,4 @@
 
-<?php include "db_set.php";
-//$iletisim = $DB_connect ->prepare("SELECT * FROM iletisim");
-//$iletisim ->execute();
-//$iletisim = $projeler ->fetchAll(PDO::FETCH_ASSOC);
-//?>
-
 <div class="contact" id="contact">
     <div class="container">
         <div class="w3ls-heading">
@@ -19,40 +13,55 @@
                     <input type="email"  class="email" name="Email" placeholder="Email" required="">
 
                 </div>
+
                 <div class="col-md-5 col-sm-5 contact-right agileits-w3layouts">
                     <textarea name="Message" placeholder="Mesaj" required=""></textarea>
                     <input type="submit" value="Gönder">
                 </div>
 
-
-                <?php
-                        if (isset($_POST['Ad'], $_POST['Soyad'], $_POST['Email'])){
-
-                                $ad =trim(filter_input(INPUT_POST, 'ad', FILTER_SANITIZE_STRING));
-                                $soyad =trim(filter_input(INPUT_POST, 'soyad', FILTER_SANITIZE_STRING));
-                                $email =trim(filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING));
-
-                        }
-                ?>
-
             </form>
 
 
-                <?php
+            <?php
+            if (isset($_POST['Ad'], $_POST['Soyad'], $_POST['Email'], $_POST['Mesaj'])) {
 
-                        $sorgu = $DB_connect ->prepare("INSERT INTO iletisim (iletisim_ad, iletisim_soyad, iletisim_mail, iletisim_mesaj) VALUES (:ad, :soyad, :email)");
-                        $sorgu->bindParam(1, $ad, PDO:: PARAM_STR);
-                        $sorgu->bindParam(2,$soyad, PDO::PARAM_STR);
-                        $sorgu->bindParam(3, $email, PDO::PARAM_STR);
+                $Ad = trim(filter_input(INPUT_POST, 'Ad', FILTER_SANITIZE_STRING));
+                $Soyad = trim(filter_input(INPUT_POST, 'Soyad', FILTER_SANITIZE_STRING));
+                $Email = trim(filter_input(INPUT_POST, 'Email', FILTER_SANITIZE_EMAIL));
+                $Mesaj = trim(filter_input(INPUT_POST, 'Mesaj', FILTER_SANITIZE_STRING));
 
-                        //$sorgu->execute();
-                ?>
+                if (empty($Ad) || empty($Soyad) || empty($Email) || empty($Mesaj)) {
+                    die("<p>Lütfen formu eksiksiz doldurun!</p>");
+                }
 
+                if (!filter_var($Email, FILTER_VALIDATE_EMAIL)) {
+                    die("<p>Lütfen geçerli bir e-posta adresin girin!</p>");
+                }
+
+
+                try {
+
+                    $sorgu = $DB_connect->prepare("INSERT INTO iletisim (iletisim_ad, iletisim_soyad, iletisim_mail, iletisim_mesaj) VALUES($Ad, $Soyad, $Email,$Mesaj)");
+                    $sorgu->bindParam(1, $Ad, PDO::PARAM_STR);
+                    $sorgu->bindParam(2, $Soyad, PDO::PARAM_STR);
+                    $sorgu->bindParam(3, $Email, PDO::PARAM_STR);
+                    $sorgu->bindParam(4, $Mesaj, PDO::PARAM_STR);
+
+                    $sorgu->execute();
+
+                    echo "<p>Bilgiler başarılı bir şekilde kaydedildi.</p>";
+
+                } catch (PDOException $e) {
+                    die($e->getMessage());
+                }
+            }
+
+            ?>
                         <footer class="w3-container w3-center w3-margin-top">
 
                             <a href="<?php echo $ayarcek['sosyal_twitter']; ?>" target="_blank"><i class="fa fa-twitter w3-hover-opacity"></i></a>
                             <a href="<?php echo $ayarcek['sosyal_linkedin']; ?>" target="_blank"><i class="fa fa-linkedin w3-hover-opacity"></i></a>
-
+                            <a href="<?php echo $ayarcek['sosyal_github']; ?>" target="_blank"><i class="fa fa-github w3-hover-opacity"></i></a>
                        </footer>
 
                 <div class="clearfix"> </div>
